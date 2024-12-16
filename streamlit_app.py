cb8ee40985a4901f3eb95863fa567cebff572c25
Skylar English <skylar.english@gmail.com>
2024-12-16 08:57:35 -0700

    pushing updates to ec2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import seaborn as sn
 import copy
 from openskill.models import PlackettLuce
 import streamlit as st
+st.set_page_config(layout='wide')
 
 games_df = pd.read_csv('game_reports_cumulative.csv', parse_dates=[2,3])
 games_df['players'] = games_df['players'].apply(lambda x: x.split(" "))
@@ -24,7 +25,7 @@ t = player_games_df['game_length_minutes']
 p = player_games_df['place']
 n = player_games_df['player_count']
 player_games_df['score'] = np.round(t * (1+((1-p)/(2*n-2))))
-player_games_df
+st.dataframe(player_games_df, column_config={'game_length':st.column_config.DatetimeColumn('game length')})
 
 
 model = PlackettLuce()
@@ -35,18 +36,23 @@ ranking_hist = []
 for game in games_df['players'].to_list():
     match = [(lambda x: [x])(ranking) for ranking in [player_rankings.get(key) for key in game]]
     results = model.rate(match)
-    print(player_rankings)
     ranking_hist.append(copy.deepcopy(player_rankings))
+ranking_hist = pd.DataFrame(ranking_hist)
 
 ranking_final = [(player, round(rating.mu,2), round(rating.sigma,2), round(rating.ordinal(),2)) for player, rating in sorted(player_rankings.items(), key=lambda x:x[1].ordinal(), reverse=True)]
 
-st.write(games_df[games_df['players'].apply(lambda x: 'Max' in x)])
 
-player = 'Skylar'
+player = st.selectbox('Select Player:',np.unique(player_games_df['players']))
+st.write(f"{player}'s games:")
+st.write(games_df[games_df['players'].apply(lambda x: player in x)])
+
+
 diffs = games_df[games_df['players'].apply(lambda x: player in x)].join(ranking_hist[player].apply(lambda x: x.mu))[player].diff()
 diffs.iloc[0] = games_df[games_df['players'].apply(lambda x: player in x)].join(ranking_hist[player].apply(lambda x: x.mu)).iloc[0][player] - 25
 diffs = pd.DataFrame(diffs)
 diffs['game'] = games_df[games_df['players'].apply(lambda x: player in x)]['game_played']
 diffs.set_index('game', inplace=True)
 diffs.plot(kind='bar')
-st.bar(diffs)
+st.write(f"{player}'s skill changes")
+st.write(diffs)
+st.bar_chart(diffs,width=1000,use_container_width=False)
